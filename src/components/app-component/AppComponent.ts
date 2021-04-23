8c84973f5db1a5978ed596d70b7c4daee8ac147b
Matthew Pagan <mastapegs01@gmail.com>
2021-04-23 19:49:43 -0400

    working tailwind

--- src/components/app-component/AppComponent.ts
+++ src/components/app-component/AppComponent.ts
@@ -1,12 +1,19 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js";
+import "../../tailwind.css";
+
+const { cssRules } = document.styleSheets[0];
+const tailwindCSS = Object.values(cssRules)
+  .map((rule) => rule.cssText)
+  .join("\n");
 
 @customElement("app-component")
 export class AppComponent extends LitElement {
+  static styles = css`${unsafeCSS(tailwindCSS)}`;
   render() {
     return html`
-      <div>
-        <h1>Hello, World!</h1>
+      <div class="p-24 bg-blue-200">
+        <h1 class="text-3xl">Hello, World!</h1>
       </div>
     `;
   }

e5921229e7b36fff6e00ea3489668a48cd6935de
Matthew Pagan <mastapegs01@gmail.com>
2021-04-25 09:54:28 -0400

    refactor LitWind logic

--- src/components/lit-wind/LitWind.ts
+++ src/components/lit-wind/LitWind.ts
@@ -2,14 +2,15 @@ import { LitElement, css, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js";
 import "../../tailwind.css";
 
-const { cssRules: tailwindCSSRules } = document.styleSheets[0];
-const tailwindCSSString = Object.values(tailwindCSSRules)
-  .map((rule) => rule.cssText)
-  .join("\n");
-
 @customElement("lit-wind")
 export class LitWind extends LitElement {
+  static getTailwindString() {
+    const { cssRules: tailwindCSSRules } = document.styleSheets[0];
+    return Object.values(tailwindCSSRules)
+      .map((rule) => rule.cssText)
+      .join("\n");
+  }
   static styles = css`
-    ${unsafeCSS(tailwindCSSString)}
+    ${unsafeCSS(LitWind.getTailwindString())}
   `;
 }
